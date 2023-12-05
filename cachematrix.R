3ff27d1059e54873524567226e2dd72abcef4c53
Ashish32700 <91833254+Ashish32700@users.noreply.github.com>
2023-12-05 16:10:46 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,24 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+# Initialize an empty cache
+  inverse <- NULL
+  
+  # Function to set the matrix
+  set <- function(newValue) {
+    x <<- newValue  # Assign the input matrix to 'x'
+    inverse <<- NULL  # Reset the cached inverse
+  }
+  
+  # Function to get the matrix
+  get <- function() {
+          #kaise bhi kralo
 }
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+         inverse <- solve(x)  # Calculate the inverse of the matrix
+  
+  return(inverse)
 }
